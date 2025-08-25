fe6c1c5af1516073e8c95c7947c2df0843fdf0eb
MateusSNeubarth <mateusneubarth@hotmail.com>
2025-08-25 15:53:20 -0300

    feat: created user model

---
+++ api/bookshelf/src/main/java/br/com/mateusneubarth/bookshelf/model/UserModel.java
@@ -0,0 +1,56 @@
+package br.com.mateusneubarth.bookshelf.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users_tab")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Long id;
+    @Column(length = 50, nullable = false)
+    private String name;
+    @Column(length = 50, nullable = false)
+    private String username;
+    @Column(length = 100, nullable = false)
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
