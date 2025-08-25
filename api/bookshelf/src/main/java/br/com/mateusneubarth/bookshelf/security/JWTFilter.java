07db4764fde4c4cada541a7762f0533cb5a6a81c
MateusSNeubarth <mateusneubarth@hotmail.com>
2025-08-25 11:52:35 -0300

    feat: jwt filter

--- api/bookshelf/src/main/java/br/com/mateusneubarth/bookshelf/security/JWTFilter.java
+++ api/bookshelf/src/main/java/br/com/mateusneubarth/bookshelf/security/JWTFilter.java
@@ -1,4 +1,44 @@
 package br.com.mateusneubarth.bookshelf.security;
 
-public class JWTFilter {
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JWTFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String token = request.getHeader(JWTCreator.HEADER_AUTHORIZATION);
+        try {
+            if (token != null && !token.isEmpty()) {
+                JWTObject tokenObject = JWTCreator.create(token, SecurityConfig.PREFIX, SecurityConfig.KEY);
+
+
+                UsernamePasswordAuthenticationToken userToken = new UsernamePasswordAuthenticationToken(
+                        tokenObject.getSubject(),
+                        null
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(userToken);
+            } else {
+                SecurityContextHolder.clearContext();
+            }
+            filterChain.doFilter(request, response);
+        } catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException e) {
+            e.printStackTrace();
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            return;
+        }
+    }
 }
